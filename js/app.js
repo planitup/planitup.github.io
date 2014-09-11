8e4bb6a5019c59aa0702b3029ddfd94a6018524e
Mitch Herrema <mherrema@yahoo.com>
2014-09-10 23:32:06 -0400

    Added chat, angularjs, and angular-strap

---
+++ js/app.js
@@ -0,0 +1,8 @@
+var planitup = angular.module('planitup', ['mgcrea.ngStrap', 'ngAnimate', 'ngSanitize']);
+
+planitup.controller("MainCtrl", function ($scope) {
+    $scope.aside = {
+        "title": "Chat",
+        "content": ' '
+    };
+});
