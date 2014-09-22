40fce05aca807cb6c5ab03aa631bde5192da66f8
Chelsea Frame <framech@mail.gvsu.edu>
2014-09-22 11:49:40 -0400

    Working on login page

--- js/app.js
+++ js/app.js
@@ -9,7 +9,44 @@ planitup.controller("MainCtrl", function ($scope) {
 });
 
 planitup.controller("loginController", function ($scope, $window) {
+	var BLANK_INPUT_CLASS = "blank";
+    var USERNAME_DEFAULT = "Username ";
+    var PASSWORD_DEFAULT = "Password ";
+    $scope.username = USERNAME_DEFAULT;
+    $scope.password = PASSWORD_DEFAULT;
+    var usernameInput = document.getElementById("userName");
+    var passwordInput = document.getElementById("passwordinput");
+	
     $scope.loginComplete = function(){
         $window.location.href = "index.html";
     };
+	
+	$scope.login = function() {
+	      if(usernameInput.value != "" && usernameInput.value != USERNAME_DEFAULT && 
+                passwordInput.value != "" && passwordInput.value != PASSWORD_DEFAULT) {
+
+                $scope.username = usernameInput.value;
+                $scope.password = passwordInput.value;
+
+                var authURL = configService.firstBaseURL + "auth?username=" + $scope.username + "&password=" + $scope.password + 
+                    "&format=jsonp&callback=JSON_CALLBACK";
+
+                $http.jsonp(authURL).then(
+                    function (response) {
+                        authService.userAuthorized = true;
+						$window.location.href = "index.html";
+                        //$location.path('/home');
+                    },
+                    function (response) {
+                        alertService.alert("There was a problem logging in.  Please try again.", "Oops!")
+                    }
+                );
+            }
+            else {
+                alertService.alert("Please enter both your username and password.", "Oops!")
+            }
+	
+	
+	};
+	
 });
